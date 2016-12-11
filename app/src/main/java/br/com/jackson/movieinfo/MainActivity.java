b603ed9c70b419297af45c2e13fb99aa1ff7cfe9
jacksoncastro <jack.vasc@yahoo.com.br>
2016-12-11 20:27:12 -0200

    Add loading dialog

--- app/src/main/java/br/com/jackson/movieinfo/MainActivity.java
+++ app/src/main/java/br/com/jackson/movieinfo/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.jackson.movieinfo;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
--- app/src/main/java/br/com/jackson/movieinfo/asynctask/PopularMovieAsyncTask.java
+++ app/src/main/java/br/com/jackson/movieinfo/asynctask/PopularMovieAsyncTask.java
@@ -1,5 +1,6 @@
 package br.com.jackson.movieinfo.asynctask;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v7.widget.LinearLayoutManager;
@@ -14,6 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.jackson.movieinfo.Constants;
+import br.com.jackson.movieinfo.MainActivity;
 import br.com.jackson.movieinfo.R;
 import br.com.jackson.movieinfo.adapter.MovieAdapter;
 import br.com.jackson.movieinfo.helper.HttpHelper;
@@ -28,10 +30,17 @@ public class PopularMovieAsyncTask extends AsyncTask<Integer, Void, List<Movie>>
 
     private final Context context;
     private final View view;
+    private ProgressDialog dialog;
 
     public PopularMovieAsyncTask(Context context, View view) {
         this.context = context;
         this.view = view;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        this.dialog = ProgressDialog.show(context, context.getString(R.string.dialog_title), context.getString(R.string.dialog_message), true);
     }
 
     @Override
@@ -60,5 +69,8 @@ public class PopularMovieAsyncTask extends AsyncTask<Integer, Void, List<Movie>>
         // save clicked movie
         MovieAdapter myRecyclerViewAdapter = new MovieAdapter(context, movies, true);
         recyclerView.setAdapter(myRecyclerViewAdapter);
+
+        // closing dialog
+        dialog.cancel();
     }
 }
